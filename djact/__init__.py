dbc978a12e2d71d760db72cd40c11291d7751725
ankurjhaaa <kumarankur7258@gmail.com>
2026-05-05 22:10:24 +0530

    bump version to 2.0.2 in __init__.py and pyproject.toml

--- djact/__init__.py
+++ djact/__init__.py
@@ -7,4 +7,4 @@ default_app_config = "djact.apps.DjactConfig"
 from djact.views import djact_endpoint  # noqa: E402, F401
 
 __all__ = ["djact_endpoint"]
-__version__ = "2.0.1"
+__version__ = "2.0.2"
